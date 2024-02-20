38cc7c819bd1e614c249213ec70aecfa69cdaeb2
DangKhoa2003 <tranhadangkhoa2003@gmail.com>
2024-02-20 20:21:27 +0700

    home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,37 +6,34 @@ import { ClerkProvider } from '@clerk/nextjs';
 import './globals.css';
 
 const IBMPlex = IBM_Plex_Sans({
-      subsets: ['latin'],
-      weight: ['400', '500', '600', '700'],
-      variable: '--font-ibm-plex',
+    subsets: ['latin'],
+    weight: ['400', '500', '600', '700'],
+    variable: '--font-ibm-plex',
 });
 
 export const metadata: Metadata = {
-      title: 'Imaginify',
-      description: 'AI-powered image generator',
+    title: 'Imaginify',
+    description: 'AI-powered image generator',
 };
 
 export default function RootLayout({
-      children,
+    children,
 }: Readonly<{
-      children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-      return (
-            <ClerkProvider
-                  appearance={{
-                        variables: { colorPrimary: '#624cf5' },
-                  }}
-            >
-                  <html lang="en">
-                        <body
-                              className={cn(
-                                    'font-IBMPlex antialiased',
-                                    IBMPlex.variable,
-                              )}
-                        >
-                              {children}
-                        </body>
-                  </html>
-            </ClerkProvider>
-      );
+    return (
+        <ClerkProvider
+            appearance={{
+                variables: { colorPrimary: '#624cf5' },
+            }}
+        >
+            <html lang="en">
+                <body
+                    className={cn('font-IBMPlex antialiased', IBMPlex.variable)}
+                >
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
+    );
 }
