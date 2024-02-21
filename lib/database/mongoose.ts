8b60190ac254766f142231098977fa4fc3d3e1f3
DangKhoa2003 <tranhadangkhoa2003@gmail.com>
2024-02-21 13:08:49 +0700

    homepage search

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,4 +1,4 @@
-import mongoose, { Mongoose } from "mongoose";
+import mongoose, { Mongoose } from 'mongoose';
 
 const MONGODB_URL = process.env.MONGODB_URL;
 
@@ -18,15 +18,18 @@ if (!cached) {
 
 export const connectToDatabase = async () => {
     if (cached.conn) return cached.conn;
+
     if (!MONGODB_URL) throw new Error('Missing MONGODB_URL');
-    
 
     cached.promise =
         cached.promise ||
         mongoose.connect(MONGODB_URL, {
             dbName: 'imaginify',
             bufferCommands: false,
+            socketTimeoutMS: 10000,
+            connectTimeoutMS: 10000,
         });
+
     cached.conn = await cached.promise;
 
     return cached.conn;
